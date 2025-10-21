a25c6b03262daee9e655260eb7dd0c6f4b72b9e5
User <nikolajevs04@gmail.com>
2025-10-21 13:22:15 +0300

    izspildiju 5. uzd

---
+++ array-and-for-loops.js
@@ -0,0 +1,7 @@
+let fruit = ["apple", "banana", "cabage", "pickler"];
+
+// console.log(fruit.length)
+
+for(let i = 0; i < fruit.length; i++){
+    console.log(fruit[i])
+}
--- output.js
+++ output.js
@@ -7,11 +7,3 @@
 
 // // DONE
 
-// // 3. uzd
-
-// // is in variables.js
-
-// // 4. uzd
-
-// // is in for-loop.js
-
