3ea5837fcbc44393fff4111770433d7d06095de5
User <nikolajevs04@gmail.com>
2025-10-21 13:27:58 +0300

    did 6. uzd sadly he cant drink and drive

--- array-and-for-loops.js
+++ array-and-for-loops.js
@@ -3,5 +3,5 @@ let fruit = ["apple", "banana", "cabage", "pickler"];
 // console.log(fruit.length)
 
 for(let i = 0; i < fruit.length; i++){
-    console.log(fruit[i])
+    console.log(i+1 + ": " + fruit[i])
 }
---
+++ conditionals.js
@@ -0,0 +1,7 @@
+let personAge = 16;
+
+if(personAge < 18){
+    console.log("he cant drink and drive")
+} else{
+    console.log("he can drink and drive")
+}
